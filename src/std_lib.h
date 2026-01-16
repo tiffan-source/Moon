6757d853beff10fffc14abcd7bee71d1f100deb2
tiffan-source <zohoungbogboanlyou@gmail.com>
2026-01-16 14:43:43 +0100

    Définitions de bibliothèque de base

---
+++ src/std_lib.h
@@ -0,0 +1,14 @@
+
+#ifndef STD_LIB_H
+#define STD_LIB_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <sys/time.h>
+#include <limits.h>
+#include <string.h>
+
+#endif // STD_LIB_H
---
+++ test/test_std_lib.c
@@ -0,0 +1,21 @@
+
+#ifdef TEST
+
+#include "unity.h"
+
+#include "std_lib.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_std_lib_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement std_lib");
+}
+
+#endif // TEST
